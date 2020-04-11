3a519aaf531405207435943f0e9733aa5ffd39ae
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-11 16:12:45 +0530

    basic form added

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -12,10 +12,12 @@ export const Login = () => <Container>
             <h3>Lets create an account</h3>
 
 
+            <TextField id="e-mai" label="User Name" />
             <TextField id="e-mai" label="E-mail address" />
             <TextField id="password" label="Password" />
+            <TextField id="password" label="confirm Password" />
             <SubmitButton type='submit'>Sing Up</SubmitButton>
-            <p>Already have an account? <span className='link'>Log in</span> </p>
+            <p className='message'>Already have an account? <span className='link'>Log in</span> </p>
 
         </FormCol>
     </Section>
