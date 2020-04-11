28cd23d6b5404396b7a9920e894296b06c96db3e
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-11 13:45:28 +0530

    first commit

---
+++ src/components/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Styled from 'styled-components/macro'
+import { Container, Section, ImageSection } from './styles';
+
+
+export const Login = () => <Container>
+    <ImageSection />
+    <Section>
+        <h2>Welcome to our community</h2>
+        <h3>Lets create an account</h3>
+    </Section>
+</Container>
---
+++ src/components/styles.ts
@@ -0,0 +1,31 @@
+import Styled from 'styled-components/macro';
+
+export const Container = Styled.article`
+    
+    box-shadow: 0 2rem 2.4rem rgba(0,0,0,0.1);
+    width: 60%;
+    border-radius: 0.8rem;
+    display:flex;
+    flex-direction: colum;
+    background-color: var(--base-white-color);
+
+`
+export const ImageSection = Styled.div`
+        width: 50%;
+        background-color: var(--base-green-color);
+
+`
+export const Section = Styled.section`
+        color: #7d9d87;
+        background-color: var(--base-white-color);
+        >h2{
+            font-size: 2.1rem;
+            font-weight: 400;
+        }
+        >h3{
+            font-size: 1.4rem;
+            font-weight: 400;
+        }
+
+
+`
