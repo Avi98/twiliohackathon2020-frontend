16992598782991ccdad097615b92ed2c8ba676ea
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-12 22:39:10 +0530

    ..

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -13,7 +13,7 @@ export const Login: FC<ILogIn> = ({
 }) =>
     <>
         <TextField id="userName" label="User Name" value={state.username} onChange={(e:any)=>updateUsername(e.target.value)} />
-        <TextField id="password" label="Password"  value={state.password} onChange={(e:any)=>updatePassword(e.target.value)}/>
+        <TextField id="password" label="Password" type='password'  value={state.password} onChange={(e:any)=>updatePassword(e.target.value)}/>
         <Button label="Log In" onClick={submitSignIn} />
         <p className='message'>Don't have Account? <span className='link' onClick={updateShowSignUp}>Sign Up</span> </p>
 
--- src/components/signUp.tsx
+++ src/components/signUp.tsx
@@ -14,8 +14,8 @@ export const SignUp: React.FC<ISignUp> = ({
     state }) =>
     <>
         <TextField id="e-mail" label="User Name" value={state.username} onChange={(e: any) => updateUsername(e.target.value)} />
-        <TextField id="e-mail" label="E-mail address" value={state.email} onChange={(e: any) => updateEmail(e.target.value)} />
-        <TextField id="password" label="Password" value={state.password} onChange={(e: any) => updatePassword(e.target.value)} />
+        <TextField id="e-mail" label="E-mail address" type='email' value={state.email} onChange={(e: any) => updateEmail(e.target.value)} />
+        <TextField id="password" label="Password" type='password' value={state.password} onChange={(e: any) => updatePassword(e.target.value)} />
         <TextField id="password" label="confirm Password" value={state.confirmPassword} onChange={(e: any) => updateConfirmPassWord(e.target.value)} />
         <Button label="Sign Up" onClick={submitLoginForm} />
         <p className='message'>Already have an account? <span className='link' onClick={updateShowLogin}>Log in</span> </p>
