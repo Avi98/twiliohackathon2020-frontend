2b712bdd676fa6249111876667e8e2e13ea49692
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-26 18:17:51 +0530

    allowed jost

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -3,6 +3,7 @@ import * as Yup from 'yup';
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
 
+
 export const ProfileSchema = Yup.object({
     first_name: Yup.string()
     .max(20, 'Must be not more than 20 letters')
