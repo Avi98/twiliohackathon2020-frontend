ec349adf8d04bf1c5e7fcb204ca8ad597b564ea8
Avinash <aviesdfsdfsdnash.kumar@instarem.com>
2020-04-26 19:33:45 +0530

    react-create-app on Heroku

---
+++ build/precache-manifest.18a5166a96e326439d5ddb1649eabc39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60dca65b978eea2d44314ffc5338cb87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aca589222d7e42347234",
+    "url": "/static/css/main.df757296.chunk.css"
+  },
+  {
+    "revision": "d03cb81c7233d7f645cf",
+    "url": "/static/js/2.707e9b2d.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.707e9b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca589222d7e42347234",
+    "url": "/static/js/main.97a5abec.chunk.js"
+  },
+  {
+    "revision": "5320eb2e37ac75f9e428",
+    "url": "/static/js/runtime-main.8372dfc9.js"
+  },
+  {
+    "revision": "782adc2b6062ab00461359da5b02b753",
+    "url": "/static/media/default-profile.782adc2b.png"
+  }
+]);
